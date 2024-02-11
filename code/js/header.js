a4409b56e1cda12757d5de8973c894c38cf346cf
yukiiso <yukiiso@student.ubc.ca>
2024-02-11 00:07:29 -0800

    Modified design of login, register, and header

--- code/js/header.js
+++ code/js/header.js
@@ -1,5 +1,5 @@
 // Adjust the height when you modify header.html 
 
 document.write(`
-    <iframe src="header.html" width = 100% height="260px" frameborder="0"></iframe>
+    <iframe src="header.html" width = 100% height="300px" frameborder="0"></iframe>
 `);
