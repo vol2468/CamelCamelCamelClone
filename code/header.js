5bddac267b00b622b879e65621ab6d4c938c6e2a
yukiiso <yukiiso@student.ubc.ca>
2024-02-10 17:22:50 -0800

    Added header.html, header.js and modified login.html so that it calls header.js

---
+++ code/header.js
@@ -0,0 +1,24 @@
+// Copy paste your header.html within document.write(` content here `) as you modify it. 
+
+document.write(`
+
+    <!DOCTYPE html>
+        <html>
+            <head lang="en">
+                <meta charset="utf-8">
+                <title>Header</title>
+            </head>
+
+            <body>
+                <header>
+                    <h1>Camelcamelcamel Clone</h1>
+                    <img src="img/logo.png" alt="Logo">
+
+                </header>
+            </body>
+
+        </html>
+
+
+
+`);
