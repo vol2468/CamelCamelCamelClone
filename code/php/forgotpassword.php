b021acb7e683d93f4a2f57ee82c46926a8236bf8
vol2468 <vol2468@student.ubc.ca>
2024-03-25 23:20:50 -0700

    other minor changes

---
+++ code/php/forgotpassword.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+// Handle forgot password
+
+// email check
+
+echo "<p>Not implemented yet.</p>";
+echo "<a href='login.php'>Go back</a>"
+
+
+
+
+?>
+
+</html>
---
+++ code/php/topdrop.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+
+echo "<h1>Coming soon...</h1>";
+echo "<a href='login.php'>Go back</a>"
+
+
+
+
+?>
+
+</html>
