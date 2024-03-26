62316224b6142dce9d57e34635c5983eb06e13c6
yukiiso <yukiiso@student.ubc.ca>
2024-03-26 01:23:45 -0700

    fixed ddl_image connect config

--- code/php/ddl_images.php
+++ code/php/ddl_images.php
@@ -8,9 +8,6 @@ try {
     // database connection
     include 'connect.php';
     
-    $connection = mysqli_connect($host, $user, $password, $database);
-    
-    $error = mysqli_connect_error();
 			
     if($error != null) {
         $output = "<p>Unable to connect to database!</p>";
