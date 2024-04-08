0258978cfca5f32b34fe215f0419c53c52235f18
yukiiso <yukiiso@student.ubc.ca>
2024-04-08 14:30:06 -0700

    Deleted unused test table

--- init.sql/ddl.sql
+++ init.sql/ddl.sql
@@ -2,29 +2,6 @@
 -- This file is automatically run when you run docker-compose up -d on your terminal. 
 
 
--- Following is the DDLe statements only for testing purposes. 
--- Actual statements will be very defferent. 
-
-DROP TABLE IF EXISTS test;
-
-CREATE TABLE test (
-    eno CHAR(5), 
-    ename VARCHAR(30) NOT NULL, 
-    bdate DATE, 
-    title CHAR(2), 
-    salary DECIMAL(9,2), 
-    supereno CHAR(5), 
-    dno CHAR(5), 
-    PRIMARY KEY (eno)
-);
-
-INSERT INTO test (eno, ename) VALUES ("E0001", "Yuki Isomura"); 
-INSERT INTO test (eno, ename) VALUES ("E0002", "Taii Hirano"); 
-INSERT INTO test (eno, ename) VALUES ("E0003", "Adams Chen"); 
-INSERT INTO test (eno, ename) VALUES ("E0004", "Mr.Random"); 
-
-
-
 -- Dropping existing tables to clear
 DROP TABLE IF EXISTS review;
 DROP TABLE IF EXISTS watchList;
