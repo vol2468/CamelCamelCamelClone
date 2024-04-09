fdeb69e145ceb85c573dacb6fa94d076e417f28b
vol2468 <vol2468@student.ubc.ca>
2024-04-09 16:40:03 -0700

    tickets page

--- code/php/dashboard.php
+++ code/php/dashboard.php
@@ -52,7 +52,7 @@ if (!isset($_SESSION["uid"])) {
                     echo "<a href='dashboard.php'>Dashboard</a>";
                     echo "<a href=''>Products</a>";
                     echo "<a href='users.php'>Users</a>";
-                    echo "<a href=''>Tickets</a>";
+                    echo "<a href='tickets.php'>Tickets</a>";
                 }
             ?>
             <a href="logout.php" id="logout">Sign out</a>
--- code/php/tickets.php
+++ code/php/tickets.php
@@ -18,9 +18,9 @@ if (!isset($_SESSION["uid"])) {
 
 <head lang="en">
     <meta charset="utf-8">
-    <title>Users</title>
+    <title>Tickets</title>
     <link rel="stylesheet" href="../css/reset.css" />
-    <link rel="stylesheet" href="../css/users.css" />
+    <link rel="stylesheet" href="../css/tickets.css" />
     <link href='https://fonts.googleapis.com/css?family=Alata' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=DM Sans' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -53,10 +53,10 @@ if (!isset($_SESSION["uid"])) {
             ?>
             <a href="logout.php" id="logout">Sign out</a>
         </div>
-        <div id="non-adminuser">
-            <div id="users">
-                <form action="users.php" method="post" id="searchuser">
-                    <input id="search" name="searchText" type="text" placeholder="Look up by username or email">
+        <div id="tickets">
+            <div id="inquiries">
+                <form action="tickets.php" method="post" id="searchuser">
+                    <input id="search" name="searchText" type="text" placeholder="Look up by email or date (YYYY-MM-DD)">
                 </form>
                 <?php
                     // using try catch statement to handle any error
@@ -69,31 +69,31 @@ if (!isset($_SESSION["uid"])) {
                             exit($output);
                         } else {
                             // check if the username is valid using a prepared statement
-                            $nonadmin = 0;
                             if (isset($_POST["searchText"]) && !empty($_POST["searchText"])) {
                                 $search = $_POST["searchText"];
-                                $sql = "SELECT uid, uname, email, usertype, i.imgid, file FROM user u LEFT JOIN image i ON u.imgid = i.imgid WHERE usertype = ? AND (uname = ? OR email = ?)";
+                                $like = "%".$_POST["searchText"]."%";
+                                $sql = "SELECT email, type, date, status FROM inquiry WHERE email = ? OR date LIKE ?";
 
                                 if ($statement = mysqli_prepare($connection, $sql)) {
-                                    mysqli_stmt_bind_param($statement, "iss", $nonadmin, $search, $search);
+                                    mysqli_stmt_bind_param($statement, "ss", $search, $like);
                                     mysqli_stmt_execute($statement);
                                     mysqli_stmt_store_result($statement);
 
                                     if (mysqli_stmt_num_rows($statement) < 1) {
-                                        echo "<p>There is no user with username \"".$search."\" or email \"".$search."\"<p>";
+                                        echo "<p>There is no inquiry with email \"".$search."\" or date \"".$search."\"<p>";
                                     } else {
-                                        echo "<table id='userlist'>";
-                                        echo "<tr class='center-header'><th>Profile Image</th><th>Username</th><th>Email</th><th>Action</th></tr>";
+                                        echo "<table id='inquiry-list'>";
+                                        echo "<tr class='center-header'><th>Email</th><th>Datetime</th><th>Type</th><th>Status</th></tr>";
                                     
                                         // fetch and display the result
-                                        mysqli_stmt_bind_result($statement, $userid, $uname, $email, $usertype, $imgid, $file);
+                                        mysqli_stmt_bind_result($statement, $email, $type, $datetime, $status);
 
                                         while (mysqli_stmt_fetch($statement)) {
                                             echo "<tr>";
-                                            echo "<td class='col-20'><img src='data:image/jpeg;base64," . base64_encode($file) . "'></td>";
-                                            echo "<td class='col-13'>" . $uname . "</td>";
-                                            echo "<td class='col-62'>" . $email . "</td>";
-                                            echo "<td class='col-5'><form action='deleteuser.php' method='post'><input type='hidden' name='uid' value='$userid'><input type='hidden' name='imgid' value='$imgid'><input id='delete' type='submit' value='Delete'></form></td>";
+                                            echo "<td class='col-30'>" . $email . "</td>";
+                                            echo "<td lass='col-30'>" . $datetime . "</td>";
+                                            echo "<td class='col-20'>" . $type . "</td>";
+                                            echo "<td class='col-20'>" . $status . "</td>";
                                             echo "</tr>";
                                         }
                                         echo "</table>";
@@ -109,27 +109,33 @@ if (!isset($_SESSION["uid"])) {
                                 mysqli_close($connection);
 
                             } else {
-                                $sql = "SELECT uid, uname, email, usertype, i.imgid, file FROM user u LEFT JOIN image i ON u.imgid = i.imgid WHERE usertype = ?";
+                                $sql = "SELECT email, type, date, status FROM inquiry";
                                 if ($statement = mysqli_prepare($connection, $sql)) {
-                                    mysqli_stmt_bind_param($statement, "i", $nonadmin);
                                     mysqli_stmt_execute($statement);
                                     mysqli_stmt_store_result($statement);
 
                                     if (mysqli_stmt_num_rows($statement) < 1) {
-                                        echo "<p>No user at this time.<p>";
+                                        echo "<p>No inquiry at this time.<p>";
                                     } else {
-                                        echo "<table id='userlist'>";
-                                        echo "<tr class='center-header'><th>Profile Image</th><th>Username</th><th>Email</th><th>Action</th></tr>";
+                                        echo "<table id='inquiry-list'>";
+                                        echo "<tr class='center-header'><th>Email</th><th>Datetime</th><th>Type</th><th>Status</th></tr>";
                                     
                                         // fetch and display the result
-                                        mysqli_stmt_bind_result($statement, $userid, $uname, $email, $usertype, $imgid, $file);
+                                        mysqli_stmt_bind_result($statement, $email, $type, $datetime, $status);
 
                                         while (mysqli_stmt_fetch($statement)) {
+                                            if ($status === "completed") {
+                                                $class = $status;
+                                                $status = "Completed";
+                                            } else {
+                                                $class = "pending";
+                                                $status = "Pending";
+                                            }
                                             echo "<tr>";
-                                            echo "<td class='col-20'><img src='data:image/jpeg;base64," . base64_encode($file) . "'></td>";
-                                            echo "<td lass='col-13'>" . $uname . "</td>";
-                                            echo "<td class='col-62'>" . $email . "</td>";
-                                            echo "<td class='col-5'><form action='deleteuser.php' method='post'><input type='hidden' name='uid' value='$userid'><input type='hidden' name='imgid' value='$imgid'><input id='delete' type='submit' value='Delete'></form></td>";
+                                            echo "<td class='col-30'>" . $email . "</td>";
+                                            echo "<td lass='col-30'>" . $datetime . "</td>";
+                                            echo "<td class='col-20'>" . $type . "</td>";
+                                            echo "<td class='col-20'><span class=$class>" . $status . "</span></td>";
                                             echo "</tr>";
                                         }
                                         echo "</table>";
