04dc3e47f8faf37da557918a92c0effbcf34760a
yukiiso <yukiiso@student.ubc.ca>
2024-04-10 13:20:06 -0700

    modified db configs for uploading to server

--- code/php/connect.php
+++ code/php/connect.php
@@ -1,14 +1,13 @@
 <?php
-    // database connection
-    $host = "localhost";
-    $database = "project";
-    $user = "root";
-    $password = "";
+    // $host = "localhost";
+    // $database = "project";
+    // $user = "root";
+    // $password = "";
 
-    // $host = "cosc360.ok.ubc.ca";
-    // $database = "db_11888757";
-    // $user = "11888757";
-    // $password = "11888757";
+    $host = "cosc360.ok.ubc.ca";
+    $database = "db_11888757";
+    $user = "11888757";
+    $password = "11888757";
     
     $connection = mysqli_connect($host, $user, $password, $database);
     
